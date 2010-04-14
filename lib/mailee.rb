a7c059ad3a097e5ec66700656b6d01db4a840a61
Juan Maiz <maiz@mailee.(none)>
2010-04-14 12:17:42 -0300

    melhorando plugin e adicionando rake para sync

--- lib/mailee.rb
+++ lib/mailee.rb
@@ -6,6 +6,10 @@ module Mailee
     def self.find_by_internal_id iid
       find(:first, :params => {:internal_id => iid})
     end
+    def unsubscribe(data={})
+      #E.g. data --> {:reason => 'Trip to nowhere', :spam => false}
+      put(:unsubscribe, :unsubscribe => {:reason => 'Motivo não especificado'}.merge(data))
+    end
   end
   class List < Config
   end
@@ -22,7 +26,16 @@ module Mailee
           after_create :create_in_mailee
           after_update :update_in_mailee
           after_destroy :destroy_in_mailee
-          self.sync_options = {:email => :email, :name => :name}.merge(options) #options[:with] || :deleted_at
+          self.sync_options = {:email => :email, :name => :name, :news => :news}.merge(options) #options[:with] || :deleted_at
+          unless self.column_names.include?(self.sync_options[:email].to_s)
+            raise "Campo #{sync_options[:email]} não existe em #{new.class}."
+          end
+          unless self.column_names.include?(self.sync_options[:name].to_s)
+            self.sync_options[:name] = nil
+          end
+          unless self.column_names.include?(self.sync_options[:news].to_s)
+            self.sync_options[:news] = nil
+          end
         end
         include InstanceMethods
       end
@@ -39,41 +52,81 @@ module Mailee
       end
       
       def create_in_mailee
-        Mailee::Contact.create :email => send(sync_options[:email]), :name => send(sync_options[:name]), :internal_id => id
+        return if( sync_options[:news] and ! send(sync_options[:news])) # Não cria se houver o campo booleano e ele for falso
+        self.class.benchmark "Criando contato no Mailee" do
+          contact = Mailee::Contact.new
+          contact.internal_id = id
+          contact.email = send(sync_options[:email])
+          contact.name = send(sync_options[:name]) if sync_options[:name]
+          contact.save
+        end
       rescue
         logger.warn "MAILEE-API: Falhou ao criar o contato #{id} no Mailee"
       end
 
       def update_in_mailee
-        #TODO
+        self.class.benchmark "Atualizando contato no Mailee" do
+          contact = Mailee::Contact.find_by_internal_id id
+          if contact
+            #Se o contato existe e o booleano foi desmarcado, realiza um UNSUBSCRIBE
+            if sync_options[:news] and (! send(sync_options[:news]))
+              unsubscribe_in_mailee(contact)
+            else
+              contact.email = send(sync_options[:email])
+              contact.name = send(sync_options[:name]) if sync_options[:name]
+              contact.save
+            end
+          else
+            create_in_mailee # Se não achou o contato tem q inserir.
+          end
+        end
       rescue
         logger.warn "MAILEE-API: Falhou ao atualizar o contato #{id} no Mailee"
       end
 
-      def destroy_in_mailee
-        #TODO
+      def destroy_in_mailee contact=nil
+        self.class.benchmark "Excluindo contato no Mailee" do
+          contact ||= Mailee::Contact.find_by_internal_id id
+          contact.destroy
+        end
       rescue
-        logger.warn "MAILEE-API: Falhou ao atualizar o contato #{id} no Mailee"
+        logger.warn "MAILEE-API: Falhou ao excluir o contato #{id} no Mailee"
       end
 
+      def unsubscribe_in_mailee contact=nil
+        self.class.benchmark "Descadastrando contato no Mailee" do
+          contact ||= Mailee::Contact.find_by_internal_id id
+          contact.unsubscribe
+        end
+      rescue
+        logger.warn "MAILEE-API: Falhou ao descadastrar o contato #{id} no Mailee"
+      end
+      
       module ClassMethods
         # Sincroniza todos os itens do modelo com Mailee.
+        # Permite que se passe um datetime para enviar apenas os contatos atualizados depois desta data
         # Permite o uso de um bloco, que receberá o item do modelo e o item do Mailee associado a este.
         # Ex: Contact.send_all_to_mailee{|i,im| im.address = i.endereco; im.save }
         # Importante: este método apenas envia os contatos, mas não recebe.
         # Para receber contatos, o ideal é fazer uma exportação no Mailee e realizar uma importação deste arquivo CSV no seu sistema.
         
-        def send_all_to_mailee
-          for item in all
+        def send_all_to_mailee(after=nil)
+          items = after ? all(:conditions => ["updated_at >= ?", after]) : all
+          for item in items
             begin
               contact = Mailee::Contact.find_by_internal_id item.id
-              if contact
-                contact.email = item.send(sync_options[:email])
-                contact.name = item.send(sync_options[:name])
-                contact.save
-              else
-                contact = Mailee::Contact.create :email => item.send(sync_options[:email]), :name => item.send(sync_options[:name]), :internal_id => item.id  
+              if contact and sync_options[:news] and ! item.send(sync_options[:news])
+                contact.unsubscribe
+                yield item, contact if block_given?
+                next
               end
+              unless contact
+                contact = Mailee::Contact.new
+                contact.internal_id = item.id
+              end
+              contact.email = item.send(sync_options[:email])
+              contact.name = item.send(sync_options[:name]) if sync_options[:name]
+              contact.save
               yield item, contact if block_given?
             rescue
               logger.warn "MAILEE-API: Falhou ao enviar o contato #{id} ao Mailee"
---
+++ tasks/mailee_tasks.rake
@@ -0,0 +1,27 @@
+def obtain_class
+  class_name = ENV['CLASS'] || ENV['class']
+  raise "Deve especificar CLASS" unless class_name
+  @klass = Object.const_get(class_name)
+end
+
+def obtain_after
+  after = ENV['AFTER'] || ENV['after']
+  @after = after ? eval(after) : nil
+end
+
+namespace :mailee do
+  desc <<-DESC
+Sincroniza os items da tabela CLASS com os contatos do Mailee.
+Você pode também especificar o env AFTER para enviar apenas os contatos atualizados após uma data.
+E.g. rake mailee:send CLASS=Contact AFTER=1.day.
+  DESC
+  task :send => :environment do
+    klass = obtain_class
+    after = obtain_after
+    raise "A classe #{klass.name} deve ser sincronizada com o Mailee. Adicione o código 'sync_with_mailee'" if ! klass.syncd?
+    print "Enviando\n"
+    klass.send_all_to_mailee(after) do
+      print "."
+    end
+  end
+end
